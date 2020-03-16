19ea64cf54577c471279a58a5f936a841d4353a7
Lvzbel <rcoto@protonmail.com>
2020-03-15 22:05:20 -0700

    Feat: created new views under vehicle folder

---
+++ resources/views/vehicles/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('content')
+<h1>All Vehicles</h1>
+
+    <ul>
+      @foreach ($vehicles as $vehicle)
+    <h3>Work Order: {{ $vehicle->work_order }}</h3>
+    <p>Date: {{ $vehicle->created_at->format('M-d') }}</p>
+      @endforeach
+    </ul>
+@endsection
