ae7932ae092a3b31e148690106a7e8726cb46e90
Lvzbel <rcoto@protonmail.com>
2020-03-18 21:52:39 -0700

    Fix: view vehicle.index edit link fixed

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -7,6 +7,7 @@
       @foreach ($vehicles as $vehicle)
     <h3>Work Order: {{ $vehicle->work_order }}</h3>
     <p>Date: {{ $vehicle->created_at->format('M-d') }}</p>
+    <a href="/vehicles/{{ $vehicle->work_order}}/edit">Edit</a>
       @endforeach
     </ul>
 @endsection
