9feb9444226364af3e6818b87fc543da1465a8a5
Lvzbel <rcoto@protonmail.com>
2020-03-18 21:38:33 -0700

    Update: mass assigment for Vehicle

--- app/Vehicle.php
+++ app/Vehicle.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehicle extends Model
 {
+    protected $guarded = [];
+    
     public function getRouteKeyName()
     {
         return 'work_order';
