25aa6159fbbb93ef8679d7e028abd5be189bad39
Lvzbel <rcoto@protonmail.com>
2020-03-18 21:47:39 -0700

    Update: Vehicle edit pages was udpated

--- resources/views/vehicles/edit.blade.php
+++ resources/views/vehicles/edit.blade.php
@@ -5,17 +5,33 @@
   <form action="/vehicles/{{ $vehicle->work_order }}" method="POST">
       @csrf
       @method('PUT')
-      <label for="work-order">Work Order</label>
-    <input name="work-order" type="text" id="work-order" value="{{ $vehicle->work_order }}">
 
-      <label for="account">Account</label>
-      <select name="account" id="account">
-        <option value="1">Vroom</option>
-        <option value="2">Hertz</option>
-      </select>
+      <div class="section">
+        <label for="work-_order">Work Order</label>
+        <input name="work_order" type="text" id="work_order" value="{{ $vehicle->work_order }}">
+        @error('work_order')
+          <p style="color: red;">{{ $errors->first('work_order')}}</p>
+        @enderror
+      </div>
 
-      <label for="notes">Notes (optional)</label>
-    <textarea name="notes" id="notes" type="text">{{ $vehicle->notes }}</textarea>
+      <div class="section">
+        <label for="account_id"></label>
+        <select name="account_id" id="account_id">
+          <option value="1">Vroom</option>
+          <option value="2">Hertz</option>
+        </select>
+        @error('account_id')
+        <p style="color: red;">{{ $errors->first('account_id')}}</p>
+      @enderror
+      </div>
+
+      <div class="section">
+        <label for="notes">Notes (optional)</label>
+        <textarea name="notes" id="notes" type="text">{{ $vehicle->notes }}</textarea>
+        @error('notes')
+        <p style="color: red;">{{ $errors->first('notes')}}</p>
+      @enderror
+      </div>
 
       <button type="submit">Submit</button>
     </form>
