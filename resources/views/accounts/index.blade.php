379dedcdbad5572f6d000ea1d23625219fa7dee8
Lvzbel <rcoto@protonmail.com>
2020-03-19 22:53:11 -0700

    Feat: Accounts.index view is renders all accounts name

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -2,4 +2,11 @@
 
 @section('content')
     <h1>All Accounts</h1>
+
+    <ul>
+    @foreach ($accounts as $account)
+    <li> {{ $account->account_name }}</li>
+    @endforeach
+  </ul>
+
 @endsection
